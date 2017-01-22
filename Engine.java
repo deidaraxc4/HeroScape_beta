c6b388347c1f8568d8036c60d902c26d91946148
deidaraxc4 <tangnick@live.com>
2017-01-21 19:26:03 -0500

    Update Engine.java

--- Engine.java
+++ Engine.java
@@ -7,31 +7,15 @@ import java.util.*;
  *
  */
 public class Engine {
+	Hero hero1 = null;
+	Hero hero2 = null;
 	
 	public static void main(String[] args) {
-		Hero hero1 = null;
-		Hero hero2 = null;
+		
 		intro();
 		//Players select the class
-		Scanner scanner = new Scanner(System.in);
-        System.out.println("Player 1 select a class: Warrior, Mage, Ranger ");
-			String p = scanner.next();
-	        if(p.equals("Warrior")) {
-	        	hero1 = new Warrior(120,15,5);
-	        } else if(p.equals("Mage")) {
-	        	hero1 = new Mage(100,5,100,20);
-	        } else if(p.equals("Ranger")) {
-	        	hero1 = new Ranger(110,0,10,5);
-	        }
-        System.out.println("Player 2 select a class: Warrior, Mage, Ranger ");
-	        String p1 = scanner.next();
-	        if(p1.equals("Warrior")) {
-	        	hero2 = new Warrior(120,15,5);
-	        } else if(p1.equals("Mage")) {
-	        	hero2 = new Mage(100,5,100,20);
-	        } else if(p1.equals("Ranger")) {
-	        	hero2 = new Ranger(110,0,10,5);
-	        }
+		heroSelect1();
+		heroSelect2();
         //battle simulator
 	        
 		
@@ -45,6 +29,7 @@ public class Engine {
         System.out.println("There are three different hero classes: Warrior, Mage, Ranger");
         System.out.println("Each have their own unique stats and special abilities.");
         System.out.println("For help with commands type in 'help'");
+        System.out.println();
     }
 	
 	public static int rngGen(int min, int max) {
@@ -53,19 +38,34 @@ public class Engine {
 	
 	public void displayOptions(Hero player) {
 		//How to check what subclass the superclass player is?
+		if(hero1.getClass()==Mage) {//this doesn't work
+			//display mage options
+		}
 	}
 	
-//	public static void heroSelect1() {
-//		Scanner scanner = new Scanner(System.in);
-//		String p = scanner.next();
-//        if(p.equals("Warrior")) {
-//        	hero1 = new Warrior(120,15,5);
-//        } else if(p.equals("Mage")) {
-//        	hero1 = new Mage(100,5,100,20);
-//        } else if(p.equals("Ranger")) {
-//        	hero1 = new Ranger(110,0,10,5);
-//        }
-//	}
+	public void heroSelect1() {
+		Scanner scanner = new Scanner(System.in);
+		String p = scanner.next();
+        if(p.equals("Warrior")) {
+        	hero1 = new Warrior(120,15,5);
+        } else if(p.equals("Mage")) {
+        	hero1 = new Mage(100,5,100,20);
+        } else if(p.equals("Ranger")) {
+        	hero1 = new Ranger(110,0,10,5);
+        }
+	}
+	
+	public void heroSelect2() {
+		Scanner scanner = new Scanner(System.in);
+		String p = scanner.next();
+        if(p.equals("Warrior")) {
+        	hero2 = new Warrior(120,15,5);
+        } else if(p.equals("Mage")) {
+        	hero2 = new Mage(100,5,100,20);
+        } else if(p.equals("Ranger")) {
+        	hero2 = new Ranger(110,0,10,5);
+        }
+	}
 	
 	
 }
